14f9b220121a9bc0d30643bb1c6bc58ba2ce984f
Hugo MOCHET <mochet.hugo@gmail.com>
2017-04-27 13:56:21 +0200

    TCI can be completed

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from tcidatabase.models import Token
 from tcidatabase import db
+from tcidata import get_tci
+
 
 app = Flask(__name__)
 
-db.connect(db='tci', username='', password='', host='localhost',
-           port=27017)
+db.connect(db='tci', username='', password='', host='localhost', port=27017)
 
 
 @app.route('/')
@@ -19,7 +20,9 @@ def survey(token):
         verify_token = Token.objects.get(key=token, usage_date=None)
     except Token.DoesNotExist:
         return render_template('error.html')
-    return render_template('survey.html')
+    tci = get_tci('tcims')
+    questions = tci.get('questions')
+    return render_template('survey.html', questions=questions)
 
 
 @app.route('/end', methods=['post'])
